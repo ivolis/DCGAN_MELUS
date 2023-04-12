58e888d2801183e88ac3f9e69f1068fd044dab91
ivolis <ilisman@fi.uba.ar>
2023-04-12 12:20:48 -0300

    minor change

--- FID_utils/load_data.py
+++ FID_utils/load_data.py
@@ -11,8 +11,8 @@ def load_data_from_dirs(real_dir, fake_dir):
     fake_filenames = [file_name for file_name in os.listdir(fake_dir) if os.path.isfile(os.path.join(fake_dir, file_name))]
 
     # loading an image in order to avoid hard-coding the heigh, width and channel
-    real_aux_img = cv2.imread(real_dir + real_filenames[0])
-    fake_aux_img = cv2.imread(fake_dir + fake_filenames[0])
+    real_aux_img = cv2.imread(os.path.join(real_dir, real_filenames[0]))
+    fake_aux_img = cv2.imread(os.path.join(fake_dir, fake_filenames[0]))
 
     # (amount of imgs, height, width, channels)
     x_real = np.empty(shape=(len(real_filenames), real_aux_img.shape[0], real_aux_img.shape[1], fake_aux_img.shape[2]))
